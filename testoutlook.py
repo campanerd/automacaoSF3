f20fa604ef6c3c8a16857704e99ba4bc3b53ad3d
Davi Campaner <campanerdavi@gmail.com>
2026-01-20 18:14:41 -0300

    change: use .env variables

--- testoutlook.py
+++ testoutlook.py
@@ -1,4 +1,7 @@
 import win32com.client as win32
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Abre o Outlook
 outlook = win32.Dispatch('Outlook.Application')
@@ -7,7 +10,8 @@ outlook = win32.Dispatch('Outlook.Application')
 mail = outlook.CreateItem(0)
 
 # Configurações básicas
-mail.To = "davi.fernandes@icrcobranca.com.br"      
+EMAIL_TO = os.getenv("EMAIL_TO")
+mail.To = EMAIL_TO 
 mail.Subject = "Teste de envio automático - Python"
 mail.Body = (
     "Olá,\n\n"
